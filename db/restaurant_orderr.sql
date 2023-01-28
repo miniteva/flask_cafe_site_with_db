b13315e11c1bf47cbb4cd9fd7fe0096ec2a8c9e8
miniteva <123831252+miniteva@users.noreply.github.com>
2023-01-28 21:44:14 +0300

    Add files via upload

---
+++ db/restaurant_orderr.sql
@@ -0,0 +1,57 @@
+-- MySQL dump 10.13  Distrib 8.0.23, for Win64 (x86_64)
+--
+-- Host: localhost    Database: restaurant
+-- ------------------------------------------------------
+-- Server version	5.6.22-log
+
+/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
+/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
+/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
+/*!50503 SET NAMES utf8 */;
+/*!40103 SET @OLD_TIME_ZONE=@@TIME_ZONE */;
+/*!40103 SET TIME_ZONE='+00:00' */;
+/*!40014 SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0 */;
+/*!40014 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0 */;
+/*!40101 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='NO_AUTO_VALUE_ON_ZERO' */;
+/*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
+
+--
+-- Table structure for table `orderr`
+--
+
+DROP TABLE IF EXISTS `orderr`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!50503 SET character_set_client = utf8mb4 */;
+CREATE TABLE `orderr` (
+  `OR_id` int(11) NOT NULL,
+  `S_id` int(11) NOT NULL,
+  `officiant_id` int(11) NOT NULL,
+  `datakogda` int(11) NOT NULL,
+  `stomostb` float DEFAULT NULL,
+  PRIMARY KEY (`OR_id`),
+  KEY `stolgde_idx` (`S_id`),
+  KEY `ktoprinial_idx` (`officiant_id`),
+  CONSTRAINT `ktoprinial` FOREIGN KEY (`officiant_id`) REFERENCES `officiant` (`officiant_id`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `stolgde` FOREIGN KEY (`S_id`) REFERENCES `stol` (`S_id`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
+-- Dumping data for table `orderr`
+--
+
+LOCK TABLES `orderr` WRITE;
+/*!40000 ALTER TABLE `orderr` DISABLE KEYS */;
+/*!40000 ALTER TABLE `orderr` ENABLE KEYS */;
+UNLOCK TABLES;
+/*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
+
+/*!40101 SET SQL_MODE=@OLD_SQL_MODE */;
+/*!40014 SET FOREIGN_KEY_CHECKS=@OLD_FOREIGN_KEY_CHECKS */;
+/*!40014 SET UNIQUE_CHECKS=@OLD_UNIQUE_CHECKS */;
+/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
+/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
+/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+/*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
+
+-- Dump completed on 2022-03-18 17:28:06
